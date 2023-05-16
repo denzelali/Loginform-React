6c172bc909d53c083a6222d5ccd8cca9663264fa
Denzel A <124642669+denzelali@users.noreply.github.com>
2023-05-15 19:10:45 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Box, TextField, Button } from '@mui/material'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+
+
+    <div>
+      <Box sx={{display:'flex', flexDirection: 'column'}}>
+      <h4>Login Form</h4>
+
+        <TextField size="small"
+          label="Username"
+        />
+        <TextField size="small"
+          label="Password" sx={{mt: 1}}
+        />
+        <Button variant='contained' sx={{mt: 1}}>
+          Login
+        </Button>
+        
+      </Box>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
